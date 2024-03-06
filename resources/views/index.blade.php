1b45c3c2cdfac7c908c2f50779f3b60cc5c25e98
TostadaRF <tostada.rf@gmail.com>
2024-03-06 20:33:21 +0000

    me quiero matar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="description" content=""/>
+        <meta name="author" content=""/>
         <title>Amigo</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap Icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"/>
         <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css"/>
         <!-- SimpleLightbox plugin CSS-->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet"/>
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{asset('css/index.css')}}" rel="stylesheet" />
+        <link href="{{asset('css/index.css')}}" rel="stylesheet"/>
     </head>
     <body id="page-top">
         <!-- Navigation-->
@@ -39,11 +39,11 @@
                 <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-8 align-self-end">
                         <h1 class="text-white font-weight-bold">Conoce gente nueva en tus sitios favoritos</h1>
-                        <hr class="divider" />
+                        <hr class="divider"/>
                     </div>
                     <div class="col-lg-8 align-self-baseline">
                         <p class="text-white-75 mb-5"> Descubre experiencias únicas que cambiarán tu vida.</p>
-                        <a class="btn btn-primary btn-xl" href="#about">Regístrate</a>
+                        <a class="btn btn-primary btn-xl" href="{{ route('register') }}">Regístrate</a>
                     </div>
                 </div>
             </div>
