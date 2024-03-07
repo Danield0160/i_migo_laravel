71ad4b8221877324755bb40e98fb8c73bd58b4a9
daniel <daniel.rosles@gmail.com>
2024-03-07 08:24:44 +0000

    correcion de vista mapa

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -8,7 +8,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script type="text/javascript" src="{{asset('js/mapa.js')}}"></script>
     <link rel="stylesheet" href="{{asset('css/mapa.css')}}">
     <title>Document</title>
 </head>
