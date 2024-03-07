f936c9ae6984b021eee772c964fc7f108448805d
daniel <daniel.rosles@gmail.com>
2024-03-07 22:26:16 +0000

    css ligero en menu

--- resources/views/partials/header_navbar_app.blade.php
+++ resources/views/partials/header_navbar_app.blade.php
@@ -1,1 +1,17 @@
-<a href="{{ route('index') }}">Volver al index</a>
+<div id="buscador">
+    &#128270;<input type="text">
+</div>
+
+<div id="menu_botones_header">
+
+    <div class="header_navbar_button">
+        <a href="{{ route('index') }}">Volver al index</a>
+    </div>
+    <div class="header_navbar_button">
+        Eventos
+    </div>
+    <div class="header_navbar_button">
+        Social
+    </div>
+
+</div>
