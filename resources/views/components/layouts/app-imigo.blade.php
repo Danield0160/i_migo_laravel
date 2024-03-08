5199bb597aa3204142e766d490c1740354a1fa99
TostadaRF <a.riverofabelo@gmail.com>
2024-03-08 10:11:15 +0000

    css

--- resources/views/components/layouts/app-imigo.blade.php
+++ resources/views/components/layouts/app-imigo.blade.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/mapa.css')}}">
     <link rel="stylesheet" href="{{asset('css/lateral.css')}}">
+    <link rel="stylesheet" href="{{asset('css/index.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"/>
     <title>Document</title>
 </head>
 <body>
@@ -32,4 +37,6 @@
     </div>
 
 </body>
+<script src="{{asset('js/menu_lateral.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
--- resources/views/partials/header_navbar_app.blade.php
+++ resources/views/partials/header_navbar_app.blade.php
@@ -1,17 +1,22 @@
-<div id="buscador">
-    &#128270;<input type="text">
-</div>
-
-<div id="menu_botones_header">
-
-    <div class="header_navbar_button">
-        <a href="{{ route('index') }}">Volver al index</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+            <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0);"><i class="bi bi-house"></i>Dashboard</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="javascript:void(0);"><i class="bi bi-calendar-event"></i>Eventos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0);"><i class="bi bi-chat"></i>Chats</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0);"><i class="bi bi-person-fill"></i>Perfil</a>
+            </li>
+        </ul>
     </div>
-    <div class="header_navbar_button">
-        Eventos
-    </div>
-    <div class="header_navbar_button">
-        Social
-    </div>
-
-</div>
+</nav>
